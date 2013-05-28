d5c2aba8df869cf45e87861a7f1c6325ffef65ac
Sascha Droste <sascha.droste@gmail.com>
2013-05-28 04:18:22 +0200

    fixed test with news translations

--- test/test-symbols.js
+++ test/test-symbols.js
@@ -5,8 +5,8 @@ describe('getSlug symbols', function () {
         getSlug('Foo & Bar | Baz').should.eql('foo-and-bar-or-baz');
         getSlug('Foo & Bar | Baz', {lang: 'en'}).should.eql('foo-and-bar-or-baz');
         getSlug('Foo & Bar | Baz', {lang: 'de'}).should.eql('foo-und-bar-oder-baz');
-        getSlug('Foo & Bar | Baz', {lang: 'fr'}).should.eql('foo-and-bar-or-baz');
-        getSlug('Foo & Bar | Baz', {lang: 'es'}).should.eql('foo-and-bar-or-baz');
+        getSlug('Foo & Bar | Baz', {lang: 'fr'}).should.eql('foo-et-bar-ou-baz');
+        getSlug('Foo & Bar | Baz', {lang: 'es'}).should.eql('foo-y-bar-u-baz');
         getSlug('Foo & Bar | Baz', {lang: 'ru'}).should.eql('foo-and-bar-or-baz');
     });
 
@@ -14,8 +14,8 @@ describe('getSlug symbols', function () {
         getSlug('Foo & Bar | Baz', {uric: true}).should.eql('foo-&-bar-or-baz');
         getSlug('Foo & Bar | Baz', {lang: 'en', uric: true}).should.eql('foo-&-bar-or-baz');
         getSlug('Foo & Bar | Baz', {lang: 'de', uric: true}).should.eql('foo-&-bar-oder-baz');
-        getSlug('Foo & Bar | Baz', {lang: 'fr', uric: true}).should.eql('foo-&-bar-or-baz');
-        getSlug('Foo & Bar | Baz', {lang: 'es', uric: true}).should.eql('foo-&-bar-or-baz');
+        getSlug('Foo & Bar | Baz', {lang: 'fr', uric: true}).should.eql('foo-&-bar-ou-baz');
+        getSlug('Foo & Bar | Baz', {lang: 'es', uric: true}).should.eql('foo-&-bar-u-baz');
         getSlug('Foo & Bar | Baz', {lang: 'ru', uric: true}).should.eql('foo-&-bar-or-baz');
     });
 
@@ -23,8 +23,8 @@ describe('getSlug symbols', function () {
         getSlug('Foo & Bar | Baz', {uricNoSlash: true}).should.eql('foo-&-bar-or-baz');
         getSlug('Foo & Bar | Baz', {lang: 'en', uricNoSlash: true}).should.eql('foo-&-bar-or-baz');
         getSlug('Foo & Bar | Baz', {lang: 'de', uricNoSlash: true}).should.eql('foo-&-bar-oder-baz');
-        getSlug('Foo & Bar | Baz', {lang: 'fr', uricNoSlash: true}).should.eql('foo-&-bar-or-baz');
-        getSlug('Foo & Bar | Baz', {lang: 'es', uricNoSlash: true}).should.eql('foo-&-bar-or-baz');
+        getSlug('Foo & Bar | Baz', {lang: 'fr', uricNoSlash: true}).should.eql('foo-&-bar-ou-baz');
+        getSlug('Foo & Bar | Baz', {lang: 'es', uricNoSlash: true}).should.eql('foo-&-bar-u-baz');
         getSlug('Foo & Bar | Baz', {lang: 'ru', uricNoSlash: true}).should.eql('foo-&-bar-or-baz');
     });
 
@@ -39,9 +39,9 @@ describe('getSlug symbols', function () {
 
    it('should convert symbols with flags true', function () {
         getSlug('Foo (♥) ; Baz=Bar', {lang: 'en', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(love)-;-baz=bar');
-        getSlug('Foo (♥) ; Baz=Bar', {lang: 'de', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(herz)-;-baz=bar');
-        getSlug('Foo (♥) ; Baz=Bar', {lang: 'fr', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(love)-;-baz=bar');
-        getSlug('Foo (♥) ; Baz=Bar', {lang: 'es', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(love)-;-baz=bar');
+        getSlug('Foo (♥) ; Baz=Bar', {lang: 'de', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(liebe)-;-baz=bar');
+        getSlug('Foo (♥) ; Baz=Bar', {lang: 'fr', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(amour)-;-baz=bar');
+        getSlug('Foo (♥) ; Baz=Bar', {lang: 'es', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(amor)-;-baz=bar');
         getSlug('Foo (♥) ; Baz=Bar', {lang: 'ru', uric: true, uricNoSlash: true, mark: true}).should.eql('foo-(love)-;-baz=bar');
    });
 
