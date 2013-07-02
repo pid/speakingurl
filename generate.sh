9eb406ac9d3feb848988eea31b0d90c3aeeedf84
Sascha Droste <sascha.droste@gmail.com>
2013-07-02 16:19:46 +0200

    removed generate script

--- generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Generates index.html from README.md of my project
-# see: http://developer.github.com/v3/markdown/#render-a-markdown-document-in-raw-mode 
-#
-RAW_README_URL=https://raw.github.com/pid/speakingurl/master/README.md
-GITHUB_API_URL=https://api.github.com/markdown/raw
-
-curl -s $RAW_README_URL | curl -s --data-binary @- -H 'Content-Type: text/plain' $GITHUB_API_URL 
