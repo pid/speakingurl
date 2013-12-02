ce559e4b0ef41a3f8d4021b9a5bec5c796ab5312
Sascha Droste <pid@posteo.net>
2013-12-02 03:04:57 +0100

    remove verbose log

--- test/test-language.js
+++ test/test-language.js
@@ -96,13 +96,13 @@ describe('getSlug languages', function() {
 
         Object.keys(symbolMap).forEach(function(l) {
 
-            console.log('\ncheck language: ' + l);
+            // console.log('\ncheck language: ' + l);
 
             Object.keys(symbolMap[l]).forEach(function(s) {
 
                 var k = symbolMap[l][s];
 
-                console.log('check symbol: ' + s);
+                // console.log('check symbol: ' + s);
 
                 getSlug('Foo ' + s + ' Bar', {
                     lang: l,
