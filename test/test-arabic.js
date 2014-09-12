bfa3b4e96e90013b6a76020b23ae6fcf337a73b6
Sascha Droste <pid@posteo.net>
2014-09-12 21:49:29 +0200

    add test for arabic transliteration

---
+++ test/test-arabic.js
@@ -0,0 +1,17 @@
+/* global describe,it */
+
+var getSlug = require('../lib');
+
+describe('getSlug translate arabic letters', function() {
+    'use strict';
+
+    it('should be ', function(done) {
+
+        getSlug('بشس تاقفغقف  -  ت ب ي ق', {
+            language: 'ar'
+        }).should.eql('bshs-taqfghqf-t-b-y-q');
+
+        done();
+    });
+
+});
