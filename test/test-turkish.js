bc6a7371a1683d6a651191c81b4cf9414338ba48
Sascha Droste <pid@posteo.net>
2014-12-07 13:10:47 +0100

    fix #43; add chars to turkish langCharMap; language specific character transliteration

---
+++ test/test-turkish.js
@@ -0,0 +1,23 @@
+/* global describe,it */
+
+var getSlug = require('../lib');
+
+describe('getSlug translate turkish letters', function() {
+    'use strict';
+
+    it('should be ', function(done) {
+
+        getSlug('ÜÄÖüäö', {
+            lang: 'tr'
+        })
+            .should.eql('uaeouaeo');
+
+        getSlug('ÜÖÄ äüö', {
+            lang: 'tr'
+        })
+            .should.eql('uoae-aeuo');
+
+        done();
+    });
+
+});
