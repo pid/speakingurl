b4180bb868edcf6efabeea41d814c46eb43ef736
Jawish Hameed <jawish@gmail.com>
2015-09-13 06:00:43 +0500

    Update test-dhivehi.js

--- test/test-dhivehi.js
+++ test/test-dhivehi.js
@@ -2,13 +2,13 @@
 
 var getSlug = require('../lib/speakingurl');
 
-describe('getSlug translate arabic letters', function () {
+describe('getSlug translate dhivehi letters', function () {
     'use strict';
 
     it('should be ', function (done) {
 
         getSlug('މއދކ ވ ރ ށ ރީތި', {
-                lang: 'ar'
+                lang: 'dv'
             })
             .should.eql('madhk-v-r-sh-reethi');
 
